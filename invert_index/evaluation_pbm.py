b5bd677c42d6f134d7ffd58f1853efba95e86e64
= <kingbolanda@live.com>
2016-03-31 22:17:45 +0200

    [#9] start evaluation

---
+++ invert_index/evaluation_pbm.py
@@ -0,0 +1,11 @@
+from pbm import PBM
+
+pbm = PBM()
+def evaluate(model_name, query_list):
+    if model_name == 'unigram':
+        pbm.
+    elif model_name == 'likelyhood':
+        pbm.
+    else:
+        pass
+
