3be9b22883dde2da54dd4195dd6e548b0ac4880d
= <kingbolanda@live.com>
2016-04-01 10:42:11 +0200

    [#9] finish evaluation script

--- invert_index/evaluation.py
+++ invert_index/evaluation.py
@@ -1,10 +1,12 @@
 from vsm import VSM
+from pbm import PBM
 import operator
 import timeit
 import csv
 import pandas as pd
 
 vsm = VSM()
+pbm = PBM()
 #evaluate models
 def evaluate(model_name, query_list):
     #simple_vsm
@@ -24,15 +26,25 @@ def evaluate(model_name, query_list):
         start = timeit.default_timer()
         res_dict = vsm.bm25_vector_space(query_list)
         stop = timeit.default_timer()
+    elif model_name == "unigram":
+        start = timeit.default_timer()
+        res_dict = pbm.BIM_rank(query_list)
+        stop = timeit.default_timer()
+    elif model_name == "likelyhood":
+        start = timeit.default_timer()
+        res_dict = pbm.log_prob(query_list)
+        stop = timeit.default_timer()
     else:
         print "no model named %s", model_name
 
     #sort res_dict by value
-    res_dict = sorted(res_dict.items(), key = operator.itemgetter(1), reverse = True)
-    return res_dict[0:20], stop - start
+    if model_name not in ['unigram', 'likelyhood']:
+        res_dict = sorted(res_dict.items(), key = operator.itemgetter(1), reverse = True)
+    return res_dict[0:30], stop - start
 
 query_term = ['chair']
 simple_dict, simple_time = evaluate("simple", query_term)
+print simple_dict
 print "simple vsm:" + str(simple_time)
 tfidf_dict, tfidf_time = evaluate("tfidf", query_term)
 print "tfidf vsm:" + str(tfidf_time)
@@ -40,7 +52,11 @@ pln_dict, pln_time = evaluate("pln", query_term)
 print "pln time:" + str(pln_time)
 bm25_dict, bm25_time = evaluate("bm25", query_term)
 print "bm25:" + str(bm25_time)
-res = pd.DataFrame({'simple': simple_dict, 'tfidf': tfidf_dict, 'pln': pln_dict, 'bm25': bm25_dict})
+unigram_dict, unigram_time = evaluate("unigram", query_term)
+print "unigram:" + str(unigram_time)
+likelyhood_dict, likelyhood_time = evaluate("likelyhood", query_term)
+print "likelyhood:" + str(likelyhood_time)
+res = pd.DataFrame({'simple': simple_dict, 'tfidf': tfidf_dict, 'pln': pln_dict, 'bm25': bm25_dict, "unigram": unigram_dict, "likelyhood": likelyhood_dict})
 res.to_csv("/home/wb/Documents/evaluation_data/e1.csv", sep = ",")
 
 
